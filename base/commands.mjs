671cfe3f31f21b91c2d5fbf66886cc709ffb1571
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-15 19:27:03 -0400

    Update commands.mjs

--- base/commands.mjs
+++ base/commands.mjs
@@ -1,1 +1,25 @@
+class Command {
+  constructor(tempName, tempCooldown, tempCategory, tempDescription, tempAction) {
+    this.name = tempName;
+    this.cooldown = tempCooldown;
+    this.category = tempCategory;
+    this.description = tempDescription;
+    this.action = tempAction;
+    this.activeCooldown = false;
+  }
+  run(tempMessage, tempArgs, tempPrefix) {
+    if(this.activeCooldown == false) {
+      this.action(tempMessage, tempArgs, tempPrefix);
+      this.activeCooldown = true;
+      console.log("chicken");
+      setTimeout(() => {
+            this.activeCooldown = false;
+            tempMessage.reply("Command cooldown finished!");
+        }, this.cooldown * 1000)
+    } else {
+      tempMessage.reply("Command cooldown active.");
+    }
+  } 
+}
 
+export default Command;
