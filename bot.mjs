a3386ce6540987b332d624bb7fd25a8163fc8669
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-01 12:06:37 -0400

    Added message event

--- bot.mjs
+++ bot.mjs
@@ -40,5 +40,37 @@ bot.on("guildDelete", async guild => {
     }
 });
 
+bot.on("message", async message => {
+    
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    
+    let prefix = data.get("prefixes");
+    if(!prefix){
+        prefix = {};
+    }
+    if(!prefix[message.guild.id]){
+        prefix[message.guild.id] = "!";
+        data.set("prefixes", prefix);
+    }
+    
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(prefix[message.guild.id])) return;
+    
+    cmd = cmd.slice(prefix.length);
+    console.log(`Command execution attempt: ${prefix}${cmd}${args}`);
+    
+    let command = commands[cmd]
+    
+    if(command){
+        command.run(message, args, prefix);
+    } else {
+        message.reply("Unknown command.");
+    }
+    message.delete();
+});
+
 const token = JSON.parse(fs.readFileSync("./token.json", "utf8"));
 bot.login(token.token);
