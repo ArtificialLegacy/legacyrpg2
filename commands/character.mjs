bb8837b441b7770178a88d314848fcfaeaad86c1
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-10 12:14:06 -0400

    Added base for command

--- commands/character.mjs
+++ commands/character.mjs
@@ -1,1 +1,11 @@
+import Command from '../base/commands.mjs';
+import settings from '../config.js';
+const Discord = settings.Discord;
+const bot = settings.bot;
+const data = settings.data;
 
+let character = new Command("character", 315, "rpg", "Creates and edits your character.", (tempMessage, tempArgs, tempPrefix) => {
+  
+});
+
+export default character;
