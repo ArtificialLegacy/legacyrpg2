2b7c68e45face590b11700ef773283df3111f3b7
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-10 12:50:01 -0400

    added something

--- commands/character.mjs
+++ commands/character.mjs
@@ -5,7 +5,10 @@ const bot = settings.bot;
 const data = settings.data;
 
 let character = new Command("character", 315, "rpg", "Creates and edits your character.", (tempMessage, tempArgs, tempPrefix) => {
-  
+  if(!tempArgs[0]){
+    message.reply("No subcommand provided!"); 
+    return;
+  }
 });
 
 export default character;
