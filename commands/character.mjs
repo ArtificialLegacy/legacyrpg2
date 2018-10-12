3cdd86b55a55cc7538b966d67bb7e244f9031d9b
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-11 20:40:19 -0400

    added callback message

--- commands/character.mjs
+++ commands/character.mjs
@@ -3,10 +3,11 @@ import settings from '../config.js';
 const Discord = settings.Discord;
 const bot = settings.bot;
 const data = settings.data;
+import callbacks from '../index/callbacks.mjs';
 
 let character = new Command("character", 315, "rpg", "Creates and edits your character.", (tempMessage, tempArgs, tempPrefix) => {
   if(!tempArgs[0]){
-    message.reply("No subcommand provided!"); 
+    message.reply(callbacks.noSubcommand); 
     return;
   }
 });
