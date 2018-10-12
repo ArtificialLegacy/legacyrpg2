7bd8aef6af4279f33b968b9417577dca7a251a07
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-11 20:37:47 -0400

    added some callbacks

--- index/callbacks.mjs
+++ index/callbacks.mjs
@@ -1,5 +1,8 @@
 let callbacks = {
-
+  "noSubCommand": "No subcommand provided!",
+  "noCommandValue": "No value provided!",
+  "commandFail": "Command execution failed!",
+  "noPermission": "No permission!",
 }
 
 export default callbacks;
