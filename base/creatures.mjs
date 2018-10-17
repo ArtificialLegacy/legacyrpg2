303031939b01e236ce7badb937be324b4f0cc6f9
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-17 12:36:12 -0400

    started to add constructor

--- base/creatures.mjs
+++ base/creatures.mjs
@@ -1,5 +1,13 @@
 class Creature {
-  
+  constructor(tempName, tempId, tempHealth, tempDamage, tempTier, tempXp=0, tempDrops=null){
+    this.name = tempName;
+    this.id = tempId;
+    this.health = tempHealth;
+    this.damage = tempDamage;
+    this.drops = tempDrops;
+    this.xp = tempXp;
+    this.tier = tempTier;
+  }
 }
 
 export default Creature;
