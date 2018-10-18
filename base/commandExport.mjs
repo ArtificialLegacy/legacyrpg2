4e542c7896836d4240aa0acee09d70d348d154ca
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-18 07:51:06 -0400

    Started to add commands

--- base/commandExport.mjs
+++ base/commandExport.mjs
@@ -1,7 +1,15 @@
-
+import botinfo from '../commands/botinfo.mjs';
+import eval from '../commands/eval.mjs';
+import help from '../commands/help.mjs';
+import profile from '../commands/profile.mjs';
+import character from '../commands/character.mjs';
 
 let commands = {
-  
+  "botinfo": botinfo,
+  "eval": eval,
+  "help": help,
+  "profile": profile,
+  "character": character,
 };
 
 export default commands;
