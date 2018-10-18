56093337cedb01406c9cb3831507995b3cc6fabd
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-18 07:56:11 -0400

    Added not a command callback

--- index/callbacks.mjs
+++ index/callbacks.mjs
@@ -4,6 +4,7 @@ let callbacks = {
   "commandFail": "Command execution failed!",
   "noPermission": "No permission!",
   "cooldownActive": "Command cooldown is still active!",
+  "notCommand": "Command not found! Use the help command for more info.",
 }
 
 export default callbacks;
