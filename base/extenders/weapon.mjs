c7ae96b966cc726c1241fd0be28394ba616462e5
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-13 20:37:12 -0500

    Added super constructor

--- base/extenders/weapon.mjs
+++ base/extenders/weapon.mjs
@@ -1,7 +1,11 @@
 import Item from '../items.mjs';
 
-class Weapon extends Item {
+const _ = undefined;
 
+class Weapon extends Item {
+  constructor(tempName, tempID, tempDescription, tempNeedsAmmo, tempBaseDurability, tempStrength, tempSellPrice, tempAmmoUsed=null, tempWeight, tempSalvageItems, tempEvent=()=>{}){
+    super(tempName, tempID, tempDescription, false, true, true, false, tempNeedsAmmo, false, true, true, false, false, "weapon", 1, 1, tempBaseDurability, _, tempStrength, _, _, _, tempSellPrice, _, tempAmmoUsed, _, tempWeight, tempSalvageItems, tempEvent);
+  }
 }
 
 export default Weapon;
