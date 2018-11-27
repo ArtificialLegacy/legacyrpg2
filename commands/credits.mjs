95b3d14946677e4af31fd6aa7cc1e032a2da3490
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-26 19:28:24 -0500

    Update credits.mjs

--- commands/credits.mjs
+++ commands/credits.mjs
@@ -1,1 +1,15 @@
+import Command from '../base/commands.mjs';
+import settings from '../config.js';
+const Discord = settings.Discord;
+const bot = settings.bot;
 
+let botinfo = new Command("botinfo", 30, "info", "Shows ping and bot information.", (tempMessage, tempArgs, tempPrefix) => {
+  let embed = new Discord.RichEmbed()
+    .setTitle("Credits")
+    .setColor("#8e278e")
+    .addField("Main Developer", "SnowflakeLegacy#0682");
+
+    tempMessage.channel.send(embed);
+});
+
+export default botinfo;
