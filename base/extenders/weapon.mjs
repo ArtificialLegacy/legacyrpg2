76f62cfcf46d1bd13521ed075bdc3916e22d6828
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-27 12:46:43 -0500

    Update weapon.mjs

--- base/extenders/weapon.mjs
+++ base/extenders/weapon.mjs
@@ -3,8 +3,8 @@ import Item from '../items.mjs';
 const _ = undefined;
 
 class Weapon extends Item {
-  constructor(tempName, tempID, tempDescription, tempNeedsAmmo, tempBaseDurability, tempStrength, tempSellPrice, tempAmmoUsed=null, tempWeight, tempSalvageItems, tempEvent=()=>{}){
-    super(tempName, tempID, tempDescription, false, true, true, false, tempNeedsAmmo, false, true, true, false, false, "weapon", 1, 1, tempBaseDurability, _, tempStrength, _, _, _, tempSellPrice, _, tempAmmoUsed, _, tempWeight, tempSalvageItems, tempEvent);
+  constructor(tempName, tempID, tempDescription, tempNeedsAmmo, tempBaseDurability, tempStrength, tempSellPrice, tempAmmoUsed=null, tempWeight, tempSalvageItems){
+    super(tempName, tempID, tempDescription, false, true, true, false, tempNeedsAmmo, false, true, true, false, false, "weapon", 1, 1, tempBaseDurability, _, tempStrength, _, _, _, tempSellPrice, _, tempAmmoUsed, _, tempWeight, tempSalvageItems);
   }
 }
 
