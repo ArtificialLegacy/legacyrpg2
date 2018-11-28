0778c58fffa45432e55a680744722216c6c319c5
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-27 19:07:52 -0500

    added super

--- src/base/extenders/ammo.mjs
+++ src/base/extenders/ammo.mjs
@@ -1,7 +1,11 @@
 import Item from '../items.mjs';
 
-class Ammo extends Item {
+const _ = undefined;
 
+class Ammo extends Item {
+  constructor(tempName, tempID, tempDescription, tempIsSellable, tempCount, tempMaxStack, tempSellPrice, tempAmmoType, tempWeight){
+    super(tempName, tempID, tempDescription, false, false, false, true, false, true, false, tempIsSellable, false, false, "ammo", tempCount, tempMaxStack, _, _, _, _, tempSellPrice, _, _, tempAmmoType, tempWeight);
+  }
 }
 
 export default Ammo;
