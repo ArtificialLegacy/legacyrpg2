76d5b0507b940dd15655e1ae92fc401a4d19e70d
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-12-06 15:11:34 -0500

    Update patch.mjs

--- src/commands/patch.mjs
+++ src/commands/patch.mjs
@@ -1,5 +1,10 @@
 import Command from '../base/commands.mjs';
 
-const patch = new Command();
+const patch = new Command("patch", 10, "info", "Shows all patch information.", (tempMessage, tempArgs, tempPrefix) => {
+  
+  if(!tempArgs){
+    
+  }
+});
 
 export default patch;
