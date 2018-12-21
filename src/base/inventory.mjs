2ffc9c1a6852c5fcba2108759a8d3bb05156aeab
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-12-21 08:54:37 -0500

    added some constructor stuff

--- src/base/inventory.mjs
+++ src/base/inventory.mjs
@@ -1,3 +1,9 @@
 class Inventory {
-
+  constructor(tempSlots){
+    this.slots = tempSlots;
+    this.inventory = [];
+    this.poke = "poke"; // <-- is very important totally :D
+  }
 }
+
+export default Inventory;
