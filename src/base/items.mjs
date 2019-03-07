f1ab76d74e6d9a22ce03165203a3c0b565a27748
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-03-07 12:49:50 -0500

    Update items.mjs

--- src/base/items.mjs
+++ src/base/items.mjs
@@ -1,14 +1,34 @@
 class Item {
- constructor(tempName, tempID, tempDescription, tempIsTool, tempIsWeapon, tempIsDurable, tempIsStackable, tempNeedsAmmo, tempIsAmmo, tempIsSalvagable, tempIsSellable, tempIsConsumable, tempIsFuel, tempType="static", tempCount=null, tempMaxStack=null, tempBaseDurability=null, tempToolStrength=null, tempWeaponStrength=null, tempCanMine=null, tempDurability=null, tempConsumableStats=[null,null], tempSellPrice=null, tempFuelPower, tempUsedAmmo=null, tempAmmoType=null, tempWeight=null, tempSalvageItems=null){
+ constructor(
+    tempName,
+    tempID,
+    tempDescription,
+    tempIsWeapon, 
+    tempIsDurable, 
+    tempIsStackable, 
+    tempNeedsAmmo, 
+    tempIsAmmo, 
+    tempIsSellable, 
+    tempIsConsumable, 
+    tempType="static", 
+    tempCount=null, 
+    tempMaxStack=null, 
+    tempBaseDurability=null, 
+    tempWeaponStrength=null, 
+    tempCanMine=null, 
+    tempDurability=null, 
+    tempConsumableStats=[null,null], 
+    tempSellPrice=null, 
+    tempUsedAmmo=null, 
+    tempAmmoType=null, 
+    tempWeight=null
+   ){
   this.name = tempName; // Item display name.
   this.id = tempID; // Item internal name;
   this.description = tempDescription; // Item details;
-  this.isTool = tempIsTool; // boolean
   this.isWeapon = tempIsWeapon; // boolean
   this.isDurable = tempIsDurable; // boolean
-  this.toolStrength = tempToolStrength; // Optional; Integer
   this.weaponStrength = tempWeaponStrength; // Optional; Integer
-  this.canMine = tempCanMine; // Optional; Array; ["wood", "minerals", "fish", "prey"]
   this.durability = tempDurability; // Optional; Integer
   this.type = tempType; // Optional; String; ["static", "weapon", "shield", "helmet", "chestplate", "gloves", "leggings", "boots", "trinket", "ammo"]
   this.event = tempEvent; // Optional; Function; Triggered when item is used;
@@ -16,7 +36,6 @@ class Item {
   this.isAmmo = tempIsAmmo; // Boolean
   this.usedAmmo = tempUsedAmmo; // Optional
   this.ammoType = tempAmmoType; // Optional
-  this.salvageItems = tempSalvageItems; // Optional; Array; Based on item ID
   this.isSellable = tempIsSellable; // Boolean
   this.sellPrice = tempSellPrice; // Optional; Integer
   this.isConsumable = tempIsConsumable; // Boolean
@@ -25,10 +44,7 @@ class Item {
   this.maxStack = tempMaxStack; // Integer; Must be positive
   this.baseDurability = tempBaseDurability; // Integer; Must be positive
   this.count = tempCount; // Integer; Must be positive
-  this.isSalvagable = tempIsSalvagable; // Boolean
   this.weight = tempWeight; // Optional; Integer; Hunger cost of equiping
-  this.isFuel = tempIsFuel; // Boolean
-  this.fuelPower = tempFuelPower; // Optional; Integer
  }
 }
 
